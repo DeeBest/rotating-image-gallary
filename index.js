41a80df7759f1f27b2b61bacf2a850ceb4b30505
DeeBest <simphiwedladla8@gmail.com>
2024-04-28 08:55:17 +0200

    added event listener to the previos image button

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const footerParagraph = document.getElementById('footer-paragraph');
 const currentDate = new Date().getFullYear();
 footerParagraph.textContent = `Created by \xA9Simphiwe ${currentDate}.`;
+
+const imagesContainer = document.getElementById('images-container');
+const prevBtn = document.getElementById('prev-btn');
+const nextBtn = document.getElementById('next-btn');
+
+let rotationDeg = 0;
+
+prevBtn.addEventListener('click', () => {
+  rotationDeg = rotationDeg + 45;
+  updateGallery();
+});
